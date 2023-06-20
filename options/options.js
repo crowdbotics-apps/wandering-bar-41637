3a61b58191d08cf4461087db2220dbe868c57dcf
Crowdbotics <team@crowdbotics.com>
2023-06-20 20:58:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-bar-41637",
+  url: "https://wandering-bar-41637.botics.co",
+  api: "https://wandering-bar-41637.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
